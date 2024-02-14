472be15f1f3f0271b881d7a8b8660687d9289fe9
ZohaibFais <murtuza.tcc@gmail.com>
2024-02-14 19:54:45 +0500

    Till Q7.

---
+++ index.js
@@ -0,0 +1,89 @@
+//Q1.
+// var num = parseFloat(prompt("Enter any positive number: "))
+// if (num < 0){
+//     alert("Enter a positive number")
+// }
+// else{
+// document.write("number:" + num + "<br>")
+// var round = Math.round(num)
+// document.write("round off value:" + round + "<br>")
+// var floor = Math.floor(num)
+// document.write("floor value:" + floor+ "<br>")
+// var ceil = Math.ceil(num)
+// document.write("ceil value:" + ceil + "<br>")
+// }
+
+// Q2.
+
+// var num = parseFloat(prompt("Enter any Negative  number: "))
+// if (num > 0){
+//     alert("Enter a negative number")
+// }
+// else{
+// document.write("number:" + num + "<br>")
+// var round = Math.round(num)
+// document.write("round off value:" + round + "<br>")
+// var floor = Math.floor(num)
+// document.write("floor value:" + floor+ "<br>")
+// var ceil = Math.ceil(num)
+// document.write("ceil value:" + ceil + "<br>")
+// }
+
+//Q3.
+
+// var num = parseFloat(prompt("Enter a positve decimal number"))
+
+// if (num < 0){
+//     alert("Enter a positive number: ")
+// }
+// else{
+// document.write("number:" + num + "<br>")
+// var round = Math.round(num)
+// document.write("round off value:" + round + "<br>")
+// var floor = Math.floor(num)
+// document.write("floor value:" + floor+ "<br>")
+// var ceil = Math.ceil(num)
+// document.write("ceil value:" + ceil + "<br>")
+// }
+
+//Q4.
+
+// var num = parseFloat(prompt("Enter a negative decimal number"))
+
+// if (num > 0){
+//     alert("Enter a negative number: ")
+// }
+// else{
+// document.write("number:" + num + "<br>")
+// var round = Math.round(num)
+// document.write("round off value:" + round + "<br>")
+// var floor = Math.floor(num)
+// document.write("floor value:" + floor+ "<br>")
+// var ceil = Math.ceil(num)
+// document.write("ceil value:" + ceil + "<br>")
+// }
+
+
+//Q5.
+
+// var num = parseFloat(prompt("Enter a number: "))
+
+// if (num == ""){
+//     alert("Enter a number")
+// }
+// else{
+//     var absolute = Math.abs(num)
+//     document.write("The absolute value of" + " " + num + " "+"is"+ " " +absolute )
+// }
+
+//Q6.
+// for (i=0; i<2; i++){
+//     var num = Math.random()
+//     var improvedNum = (num *6) + 1
+//     var diceNum = Math.floor(improvedNum)
+
+//     document.write("Random dice value: " +diceNum + "<br>")
+// }
+
+//Q7.
+
